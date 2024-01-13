7c1e6caffce7d05af327ddcfbe70a265eac0f116
mohammadSadeghAlimardani <155631923+mohammadSadeghAlimardani@users.noreply.github.com>
2024-01-13 13:09:34 +0330

    Add files via upload

--- app.js
+++ app.js
@@ -1,21 +1,3 @@
-/* ==== CUSTOMER-REVIEW ==== */
-/* ========================= */
-
-const customerRating = [...document.querySelectorAll(".customer-rating")];
-
-customerRating.map(item => {
-
-    const stars = item.dataset.satisfy;
-    let step = 0;
-    for (const element of [...item.querySelectorAll("li i")]) {
-        element.style.color = "rgb(247, 226, 15)";
-        step++;
-        if(step == stars){
-            break;
-        } 
-    }
-})
-
 /* ==== SHOW-SIDEBAR ==== */
 /* ====================== */
 
@@ -30,4 +12,94 @@ showSidebar.addEventListener("click", function(e){
 closeSidebar.addEventListener("click", function(e){
     e.preventDefault();
     sidebar.classList.remove("show");
+})
+
+/* ==== CUSTOMER-REVIEW ==== */
+/* ========================= */
+
+const customers = [
+    {
+        src : "./images/customer-image-1.jpg",
+        comment : `
+            "Its been 2 years iam using Greener to decorate my
+            house with plants and iam fully satisfied.” 
+        `,
+        satisfy : 3,
+        username : `khaled jubair abbasi`
+    },
+    {
+        src : "./images/customer-image-2.jpg",
+        comment : `
+            "very good but delivery is not fast” 
+        `,
+        satisfy : 4,
+        username : `kim min jae`
+    },
+    {
+        src : "./images/customer-image-3.jpg",
+        comment : `
+            "not bad” 
+        `,
+        satisfy : 2,
+        username : `john parker`
+    },
+    {
+        src : "./images/customer-image-4.jpg",
+        comment : `
+            "I am very satsified they are the best” 
+        `,
+        satisfy : 5,
+        username : `peter jordan`
+    },
+
+];
+
+const carouselContent = customers.map(customer => {
+
+    return `<div class="carousel-cell">      
+                <img src="${customer.src}" alt="customer-profile"/> 
+                <p class="customer-comment">                   
+                    ${customer.comment}              
+                </p>
+                <ul class="customer-rating" data-satisfy="${customer.satisfy}">    
+                    <li>
+                        <i class="fa-solid fa-star"></i>
+                    </li>
+                    <li>
+                        <i class="fa-solid fa-star"></i>
+                    </li>
+                    <li>
+                        <i class="fa-solid fa-star"></i>
+                    </li>
+                    <li>
+                        <i class="fa-solid fa-star"></i>
+                    </li>
+                    <li>
+                        <i class="fa-solid fa-star"></i>
+                    </li>   
+                </ul>
+                <h3 class="customer-username">${customer.username}</h3>
+            </div>
+    `;
+}).join("");
+
+const carousel = document.querySelector(".customer-review .carousel");
+
+carousel.innerHTML = carouselContent;
+
+// rating
+
+const customerRating = [...document.querySelectorAll(".customer-rating")];
+
+customerRating.map(item => {
+
+    const stars = item.dataset.satisfy;
+    let step = 0;
+    for (const element of [...item.querySelectorAll("li i")]) {
+        element.style.color = "rgb(247, 226, 15)";
+        step++;
+        if(step == stars){
+            break;
+        } 
+    }
 })
